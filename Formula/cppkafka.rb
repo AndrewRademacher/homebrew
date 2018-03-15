3053e20a039324850f0be6fba31275ecd4161146
AndrewRademacher <andrewrademacher@icloud.com>
2018-03-15 10:05:12 -0500

    Added cppkafka formule.

---
+++ Formula/cppkafka.rb
@@ -0,0 +1,41 @@
+class Cppkafka < Formula
+    desc "Modern C++ Apache Kafka client library (wrapper for librdkafka)"
+    homepage "https://github.com/mfontanini/cppkafka"
+    url "https://github.com/mfontanini/cppkafka/archive/v0.1.tar.gz"
+    sha256 "710e442bba8d30bb9501a98e1d4ddc2c7f78c7c3b8bfb9815c793853ab90cbe8"
+    head "https://github.com/mfontanini/cppkafka.git"
+
+    # bottle do
+    #     cellar :any
+    # end
+
+    depends_on "librdkafka"
+
+    def install
+        system "mkdir", "build"
+        system "cd", "build"
+        system "cmake", "-DCMAKE_BUILD_TYPE=Release", ".."
+        system "make"
+        system "make", "install", "DESTDIR=#{prefix}"
+    end
+
+    test do
+        (testpath/"test.cpp").write <<~EOS
+            #include <cppkafka/producer.h>
+
+            using namespace std;
+            using namespace cppkafka;
+            
+            int main() {
+                Configuration config = {
+                    { "metadata.broker.list", "127.0.0.1:9092" }
+                };
+            
+                // Create the producer
+                Producer producer(config);
+            }
+        EOS
+        system ENV.cc, "test.cpp", "-L#{lib}", "-lcppkafka", "-lrdkafka", "-lz", "-lpthread", "-o", "test"
+        system "./test"
+    end
+end
